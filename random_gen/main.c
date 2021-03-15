d963ef7c881ead2512d80cea3ad682df7f9ba5c3
Gyeongah Na <gna@c3r4s7.42seoul.kr>
2021-03-15 16:19:42 +0900

    push_swap

--- random_gen/main.c
+++ random_gen/main.c
@@ -6,7 +6,7 @@
 /*   By: gna <gna@student.42Seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 14:12:41 by gna               #+#    #+#             */
-/*   Updated: 2021/03/15 14:36:44 by gna              ###   ########.fr       */
+/*   Updated: 2021/03/15 16:15:51 by gna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,27 @@
 int	main(int argc, char **argv)
 {
 	int	num_arr[1024];
-	int	i;
 	int	j;
 	int	tmp;
 
 	srand((unsigned int)time(NULL));
-	i = 0;
-	while (i < ft_atoi(argv[1]))
+	argc = 0;
+	while (argc < ft_atoi(argv[1]))
 	{
 		tmp = rand() % ft_atoi(argv[1]) + 1;
 		j = 0;
-		while (j < i)
+		while (j < argc)
 		{
 			if (num_arr[j] == tmp)
 				break ;
 			j++;
 		}
-		if (j == i)
+		if (j == argc)
 		{
-			num_arr[i] = tmp;
+			num_arr[argc] = tmp;
 			ft_puts(ft_itoa(tmp));
 			ft_puts(" ");
-			i++;
+			argc++;
 		}
 	}
 }
